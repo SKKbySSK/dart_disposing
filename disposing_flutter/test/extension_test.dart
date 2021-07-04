00b21ec99f05c54e55cdcd2ab0fa2fe9db2d4603
Kaisei Sunaga <skkbyssk@gmail.com>
2021-07-04 19:32:48 +0900

    Add flutter extension tests

---
+++ disposing_flutter/test/extension_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:disposing_flutter/disposing_flutter.dart';
+
+class _TestChangeNotifier extends ChangeNotifier {}
+
+void main() async {
+  group('Listenable', () {
+    test('listener will be removed', () async {
+      var callCount = 0;
+      final notifier = _TestChangeNotifier();
+      final disp = notifier.addDisposableListener(() => callCount++);
+
+      notifier.notifyListeners();
+      expect(callCount, 1);
+
+      await disp.dispose();
+      notifier.notifyListeners();
+
+      expect(callCount, 1);
+    });
+
+    test('can use ChangeNotifier value', () async {
+      final notifier = _TestChangeNotifier();
+      final disp = notifier.asDisposable<_TestChangeNotifier>();
+      expect(disp.value, notifier);
+    });
+  });
+}
